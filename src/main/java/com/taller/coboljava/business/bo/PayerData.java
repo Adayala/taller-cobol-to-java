a628d9ab5de679e2e6f407e061595f8864a3f414
Adrian <adayala@santandertecnologia.com.ar>
2022-04-27 20:25:22 -0300

    Subida del refactor de PayerData

--- src/main/java/com/taller/coboljava/business/bo/PayerData.java
+++ src/main/java/com/taller/coboljava/business/bo/PayerData.java
@@ -1,8 +1,32 @@
 package com.taller.coboljava.business.bo;
 
 public class PayerData {
-    public String cbu;
-    public String name;
-    public String cardNumber;
-    public String walletId;
+    private String cbu;
+    private String name;
+    private String cardNumber;
+    private String walletId;
+
+    public PayerData (String cbu, String name, String cardNumber, String walletId) {
+        this.cbu = cbu;
+        this.name = name;
+        this.cardNumber = cardNumber;
+        this.walletId = walletId;
+    }
+
+
+    public String getCbu() {
+        return cbu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
 }
